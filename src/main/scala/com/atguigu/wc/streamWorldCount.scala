59d84144c009a5d742942bb4b1668d73e3334db0
lastingwar <yaohm7788@163.com>
2020-10-28 15:50:41 +0800

    first

---
+++ src/main/scala/com/atguigu/wc/streamWorldCount.scala
@@ -0,0 +1,28 @@
+package com.atguigu.wc
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala._
+
+/**
+ * @author yhm
+ * @create 2020-10-27 16:14
+ */
+object streamWorldCount {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val params: ParameterTool = ParameterTool.fromArgs(args)
+
+    val host: String = params.get("host")
+    val port: Int = params.getInt("port")
+    val ds: DataStream[String] = env.socketTextStream(host, port)
+
+    val value: DataStream[(String, Int)] = ds.flatMap(_.split(" "))
+      .map((_, 1))
+      .keyBy(0)
+      .sum(1)
+    value.print().setParallelism(1)
+
+    env.execute("Socket stream word count")
+  }
+}
